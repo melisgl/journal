9f9951b38796453ed9d25c74a251745d53713ddc
Gábor Melis <mega@retes.hu>
2020-09-24 22:56:08 +0100

    make slime jtrace integration fail silently if Journal is not loaded

--- src/journal.el
+++ src/journal.el
@@ -2,9 +2,15 @@
   "Toggle JTRACE."
   (interactive (list (slime-read-from-minibuffer
                       "j(un)trace: " (slime-symbol-at-point))))
-  (message "%s" (slime-eval `(journal::swank-toggle-jtrace ,spec))))
+  (message "%s" (slime-eval
+                 ;; Silently fail if Journal is not loaded.
+                 `(cl:when (cl:find-package :journal)
+                           (cl:funcall
+                            (cl:find-symbol
+                             (cl:symbol-name :swank-toggle-jtrace) :journal)
+                            ,spec)))))
 
-(define-key slime-mode-map (kbd "C-j")
+(define-key slime-mode-map (kbd "C-c C-j")
   'slime-toggle-jtrace-fdefinition)
 
 (define-key slime-repl-mode-map (kbd "C-c C-j")
